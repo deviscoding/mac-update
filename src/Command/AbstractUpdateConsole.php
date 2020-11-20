b5d9f49fd2b1e3efd372e204c9f3fddad42a19a4
AMJones <am@jonesiscoding.com>
2020-11-20 09:01:40 -0600

    Rearranges methods, adds PHPDocs

--- src/Command/AbstractUpdateConsole.php
+++ src/Command/AbstractUpdateConsole.php
@@ -1,26 +1,30 @@
 <?php
 
-
 namespace DevCoding\Mac\Update\Command;
-
 
 use DevCoding\Command\Base\AbstractConsole;
 use DevCoding\Mac\Update\Objects\MacUpdate;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Process\Process;
 
-
+/**
+ * Class AbstractUpdateConsole.
+ *
+ * @author  Aaron M Jones <aaron@jonesiscoding.com>
+ *
+ * @package DevCoding\Mac\Update\Command
+ */
 class AbstractUpdateConsole extends AbstractConsole
 {
   const PATTERN_DETAILS = '#^(.*), ([0-9]+)K\s?(.*)$#';
 
-  /** @var string */
+  /** @var string The full path to the softwareupdate binary */
   protected $_SoftwareUpdateBinary;
-  /** @var array */
+  /** @var MacUpdate[] An array of MacUpdate objects */
   protected $_Updates = [];
-  /** @var array */
+  /** @var array An array with major, minor, and revision keys, describing the MacOS version */
   protected $_DarwinVersion = [];
-  /** @var int */
+  /** @var int The number of physical CPU cores on this Mac hardware */
   protected $_Cores;
 
   // region //////////////////////////////////////////////// Symfony Command Methods
@@ -28,7 +32,7 @@ class AbstractUpdateConsole extends AbstractConsole
   protected function configure()
   {
     $this
-        ->addOption('recommended', null, InputOption::VALUE_NONE,'Only include recommended updates')
+        ->addOption('recommended', null, InputOption::VALUE_NONE, 'Only include recommended updates')
         ->addOption('restart', null, InputOption::VALUE_NONE, 'Only include restart updates')
         ->addOption('shutdown', null, InputOption::VALUE_NONE, 'Only include shutdown updates')
         ->addOption('no-scan', null, InputOption::VALUE_NONE, 'Do not scan for new updates')
@@ -38,8 +42,208 @@ class AbstractUpdateConsole extends AbstractConsole
   // endregion ///////////////////////////////////////////// End Symfony Copmmand Methods
 
   // region //////////////////////////////////////////////// Software Update Methods
+
   /**
-   * @param MacUpdate $MacUpdate
+   * @param string $label
+   * @param string $details
+   *
+   * @return MacUpdate|null
+   */
+  protected function fromCatalina($label, $details)
+  {
+    if (preg_match('#^Label:\s(.*)$#', trim($label), $lParts))
+    {
+      $Update = new MacUpdate($lParts[1]);
+
+      if (preg_match_all('#([A-Z][a-z]+):\s([^,]+),#', $details, $parts, PREG_SET_ORDER))
+      {
+        foreach ($parts as $part)
+        {
+          $key = $part[1];
+          $val = $part[2];
+
+          if ('Title' == $key)
+          {
+            $Update->setName($val);
+          }
+          if ('Size' == $key)
+          {
+            $Update->setSize($val);
+          }
+          elseif ('Recommended' == $key && 'YES' == $val)
+          {
+            $Update->setRecommended(true);
+          }
+          elseif ('Action' == $key && 'restart' == $val)
+          {
+            $Update->setRestart(true);
+          }
+          elseif ('Action' == $key && 'shut down' == $val)
+          {
+            $Update->setShutdown(true);
+          }
+        }
+      }
+
+      return $Update;
+    }
+
+    return null;
+  }
+
+  /**
+   * @param string $label
+   * @param string $details
+   *
+   * @return MacUpdate|null
+   */
+  protected function fromMojave($label, $details)
+  {
+    $Update = new MacUpdate(trim($label));
+    if (preg_match(self::PATTERN_DETAILS, $details, $parts))
+    {
+      if (!empty($parts[1]))
+      {
+        $Update->setName(trim($parts[1]));
+      }
+
+      if (!empty($parts[2]))
+      {
+        $Update->setSize($parts[2]);
+      }
+
+      if (!empty($parts[3]))
+      {
+        if (false !== strpos($parts[3], 'recommended'))
+        {
+          $Update->setRecommended(true);
+        }
+
+        if (false !== strpos($parts[3], 'restart'))
+        {
+          $Update->setRestart(true);
+        }
+
+        if (false !== strpos($parts[3], 'halt') || false !== strpos($parts[3], 'shut down'))
+        {
+          $Update->setShutdown(true);
+        }
+      }
+    }
+
+    return $Update;
+  }
+
+  /**
+   * @return string The path to the softwareupdate Binary
+   *
+   * @throws \Exception If the binary if not found
+   */
+  protected function getSoftwareUpdate()
+  {
+    if (empty($this->_SoftwareUpdateBinary))
+    {
+      $this->_SoftwareUpdateBinary = $this->getBinaryPath('softwareupdate');
+    }
+
+    return $this->_SoftwareUpdateBinary;
+  }
+
+  /**
+   * @return MacUpdate[]
+   *
+   * @throws \Exception
+   */
+  protected function getUpdateList()
+  {
+    if (empty($this->_Updates))
+    {
+      if ($suBin = $this->getSoftwareUpdate())
+      {
+        // Create Process
+        $noScan  = $this->isNoScan() ? '--no-scan' : null;
+        $Process = Process::fromShellCommandline(trim(sprintf('%s -l %s', $suBin, $noScan)));
+        // Eliminate Timeouts
+        $Process->setIdleTimeout(null)->setTimeout(null);
+        // Run the Process
+        $Process->run();
+
+        // Check for Success
+        if (!$Process->isSuccessful())
+        {
+          throw new ProcessFailedException($Process);
+        }
+        else
+        {
+          $outLines = $Process->getOutput();
+          if (empty($outLines))
+          {
+            $outLines = $Process->getErrorOutput();
+          }
+        }
+
+        $output = explode("\n", $outLines);
+        if (!empty($output))
+        {
+          $count = count($output);
+          for ($x = 0; $x < $count; ++$x)
+          {
+            unset($Update);
+            if (preg_match('#\*\s(.*)$#', trim($output[$x]), $matches))
+            {
+              if (!empty($matches[1]))
+              {
+                $xx = $x + 1;
+                if (!empty($output[$xx]))
+                {
+                  if ($this->isCatalinaUp())
+                  {
+                    $Update = $this->fromCatalina($matches[1], $output[$xx]);
+                  }
+                  else
+                  {
+                    $Update = $this->fromMojave($matches[1], $output[$xx]);
+                  }
+                }
+
+                if (isset($Update))
+                {
+                  $this->_Updates[] = $Update;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return $this->_Updates;
+  }
+
+  /**
+   * Returns an array of MacUpdate objects that match the flags given to this command instance.
+   *
+   * @return MacUpdate[]|array
+   *
+   * @throws \Exception
+   */
+  protected function getValidUpdates()
+  {
+    $MacUpdates = $this->getUpdateList();
+    $output     = [];
+    foreach ($MacUpdates as $macUpdate)
+    {
+      if ($this->isIncluded($macUpdate))
+      {
+        $output[] = $macUpdate;
+      }
+    }
+
+    return $output;
+  }
+
+  /**
+   * Determines whether the given MacUpdate object should be included in this command instance.
    *
    * @return bool
    */
@@ -49,7 +253,7 @@ class AbstractUpdateConsole extends AbstractConsole
     $isRestart     = $this->io()->getOption('restart') ? true : false;
     $isShutdown    = $this->io()->getOption('shutdown') ? true : false;
 
-    if ($isRecommended && ($MacUpdate->isRecommended() === false))
+    if ($isRecommended && (false === $MacUpdate->isRecommended()))
     {
       // Only recommended updates requested, this update isn't recommended
       return false;
@@ -71,178 +275,10 @@ class AbstractUpdateConsole extends AbstractConsole
   }
 
   /**
-   * @return MacUpdate[]|array
-   * @throws \Exception
-   */
-  protected function getValidUpdates()
-  {
-    $MacUpdates = $this->getUpdateList();
-    $output     = [];
-    foreach($MacUpdates as $macUpdate)
-    {
-      if ($this->isIncluded($macUpdate))
-      {
-        $output[] = $macUpdate;
-      }
-    }
-
-    return $output;
-  }
-
-  /**
-   * @return MacUpdate[]
-   * @throws \Exception
-   */
-  protected function getUpdateList()
-  {
-    if (empty($this->_Updates))
-    {
-      if ($suBin = $this->getSoftwareUpdate())
-      {
-        // Create Process
-        $noScan = $this->isNoScan() ? "--no-scan" : null;
-        $Process = Process::fromShellCommandline(trim(sprintf('%s -l %s', $suBin, $noScan)));
-        // Eliminate Timeouts
-        $Process->setIdleTimeout(null)->setTimeout(null);
-        // Run the Process
-        $Process->run();
-
-        // Check for Success
-        if (!$Process->isSuccessful()) {
-          throw new ProcessFailedException($Process);
-        }
-        else
-        {
-          $outLines = $Process->getOutput();
-          if (empty($outLines))
-          {
-            $outLines = $Process->getErrorOutput();
-          }
-        }
-
-        $output = explode("\n", $outLines);
-        if (!empty($output))
-        {
-          $count = count($output);
-          for($x = 0; $x < $count; $x++)
-          {
-            unset($Update);
-            if(preg_match('#\*\s(.*)$#', trim($output[$x]), $matches))
-            {
-              if (!empty($matches[1]))
-              {
-                $xx = $x+1;
-                if(!empty($output[$xx]))
-                {
-                  if ($this->isCatalinaUp())
-                  {
-                    $Update = $this->fromCatalina($matches[1], $output[$xx]);
-                  }
-                  else
-                  {
-                    $Update = $this->fromMojave($matches[1],$output[$xx]);
-                  }
-                }
-
-                if (isset($Update))
-                {
-                  $this->_Updates[] = $Update;
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    return $this->_Updates;
-  }
-
-  protected function fromCatalina($label, $details)
-  {
-    if (preg_match('#^Label:\s(.*)$#', trim($label), $lParts))
-    {
-      $Update = new MacUpdate($lParts[1]);
-
-      if (preg_match_all('#([A-Z][a-z]+):\s([^,]+),#', $details, $parts, PREG_SET_ORDER))
-      {
-        foreach($parts as $part)
-        {
-          $key = $part[1];
-          $val = $part[2];
-
-          if ($key == 'Title')
-          {
-            $Update->setName($val);
-          }
-          if ($key == 'Size')
-          {
-            $Update->setSize($val);
-          }
-          elseif ($key == 'Recommended' && $val == 'YES')
-          {
-            $Update->setRecommended(true);
-          }
-          elseif ($key == 'Action' && $val == 'restart')
-          {
-            $Update->setRestart(true);
-          }
-          elseif ($key == 'Action' && $val == 'shut down')
-          {
-            $Update->setShutdown(true);
-          }
-        }
-      }
-
-      return $Update;
-    }
-
-    return null;
-  }
-
-  /**
-   * @param string $label
-   * @param string $details
+   * If the 'no-scan' flag was used for this command instance.
    *
-   * @return MacUpdate
+   * @return bool
    */
-  protected function fromMojave($label, $details)
-  {
-    $Update = new MacUpdate(trim($label));
-    if (preg_match(self::PATTERN_DETAILS, $details, $parts))
-    {
-      if (!empty($parts[1]))
-      {
-        $Update->setName(trim($parts[1]));
-      }
-
-      if (!empty($parts[2]))
-      {
-        $Update->setSize($parts[2]);
-      }
-
-      if (!empty($parts[3]))
-      {
-        if (strpos($parts[3], 'recommended') !== false)
-        {
-          $Update->setRecommended(true);
-        }
-
-        if (strpos($parts[3], 'restart') !== false)
-        {
-          $Update->setRestart(true);
-        }
-
-        if (strpos($parts[3], 'halt') !== false || strpos($parts[3], 'shut down') !== false)
-        {
-          $Update->setShutdown(true);
-        }
-      }
-    }
-
-    return $Update;
-  }
-
   protected function isNoScan()
   {
     if ($this->io()->getInput()->hasOption('no-scan'))
@@ -253,39 +289,50 @@ class AbstractUpdateConsole extends AbstractConsole
     return false;
   }
 
-  /**
-   * @return string
-   * @throws \Exception
-   */
-  protected function getSoftwareUpdate()
-  {
-    if (empty($this->_SoftwareUpdateBinary))
-    {
-      $this->_SoftwareUpdateBinary = $this->getBinaryPath('softwareupdate');
-    }
-
-    return $this->_SoftwareUpdateBinary;
-  }
-
   // endregion ///////////////////////////////////////////// End Software Update Methods
 
   // region //////////////////////////////////////////////// System Information Functions
 
+  /**
+   * Returns the number of physical CPU cores.
+   *
+   * @return int
+   */
+  protected function getCpuCores()
+  {
+    if (empty($this->_Cores))
+    {
+      $this->_Cores = (int) shell_exec('sysctl -n hw.physicalcpu');
+    }
+
+    return $this->_Cores;
+  }
+
+  /**
+   * Returns the username of the user currently logged into the macOS GUI.
+   *
+   * @return string|null
+   */
   protected function getConsoleUser()
   {
     return $this->getShellExec("/usr/sbin/scutil <<< \"show State:/Users/ConsoleUser\" | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }'");
   }
 
+  /**
+   * Returns the ID of the user currently logged inot the macOS GUI.
+   *
+   * @return string|null
+   */
   protected function getConsoleUserId()
   {
     return $this->getUserId($this->getConsoleUser());
   }
 
-  protected function getUserId($user)
-  {
-    return $this->getShellExec(sprintf("/usr/bin/id -u %s", $user));
-  }
-
+  /**
+   * Returns an array with keys for the major, minor, and revision of MacOS currently running.
+   *
+   * @return array|int[]
+   */
   protected function getMacOsVersion()
   {
     if (empty($this->_DarwinVersion))
@@ -296,7 +343,7 @@ class AbstractUpdateConsole extends AbstractConsole
 
         $major = !empty($parts[0]) ? $parts[0] : 10;
         $minor = !empty($parts[1]) ? $parts[1] : 1;
-        $rev = !empty($parts[2]) ? $parts[2] : 0;
+        $rev   = !empty($parts[2]) ? $parts[2] : 0;
 
         $this->_DarwinVersion = ['major' => $major, 'minor' => $minor, 'revision' => $rev];
       }
@@ -305,23 +352,33 @@ class AbstractUpdateConsole extends AbstractConsole
     return $this->_DarwinVersion;
   }
 
-  protected function getCpuCores()
+  /**
+   * Returns the user id of the given username.
+   *
+   * @return string|null
+   */
+  protected function getUserId(string $user)
   {
-    if (empty($this->_Cores))
-    {
-      $this->_Cores = (int)shell_exec("sysctl -n hw.physicalcpu");
-    }
-
-    return $this->_Cores;
+    return $this->getShellExec(sprintf('/usr/bin/id -u %s', $user));
   }
 
+  /**
+   * Determines if the system is running off of battery power, or AC power.
+   *
+   * @return bool
+   */
   protected function isBatteryPowered()
   {
     $battery = $this->getShellExec('/usr/bin/pmset -g ps');
 
-    return (strpos($battery, 'Battery Power') !== false);
+    return false !== strpos($battery, 'Battery Power');
   }
 
+  /**
+   * Determines if the OS version is Catalina or greater.
+   *
+   * @return bool
+   */
   protected function isCatalinaUp()
   {
     $version = $this->getMacOsVersion();
@@ -330,6 +387,9 @@ class AbstractUpdateConsole extends AbstractConsole
   }
 
   /**
+   * Determines if the display is prevented from sleeping by an assertation, usually an indicator that a presentation
+   * or video conference is currently running.
+   *
    * @return string|null
    */
   protected function isDisplaySleepPrevented()
@@ -339,13 +399,24 @@ class AbstractUpdateConsole extends AbstractConsole
     return !empty($a);
   }
 
+  /**
+   * Determines if the OS is currently encrypting a FileVault.
+   *
+   * @return bool
+   */
   protected function isEncryptingFileVault()
   {
     $fv = $this->getShellExec('/usr/bin/fdesetup status');
 
-    return (strpos($fv, 'Encryption in progress') !== false);
+    return false !== strpos($fv, 'Encryption in progress');
   }
 
+  /**
+   * Returns an opinionated determination of whether the CPU load is 'high' based on the current load and the number of
+   * CPU cores. Loads greater than half the number of CPU cores are considered high.  This intentinoally conservative.
+   *
+   * @return bool
+   */
   protected function isLoadHigh()
   {
     if (function_exists('sys_getloadavg'))
@@ -353,12 +424,17 @@ class AbstractUpdateConsole extends AbstractConsole
       $cores = $this->getCpuCores() / 2;
       $load  = sys_getloadavg();
 
-      return isset($load[0]) && (float)$load[0] > $cores;
+      return isset($load[0]) && (float) $load[0] > $cores;
     }
 
     return false;
   }
 
+  /**
+   * Determines if the Mac has a T2 security chip.
+   *
+   * @return bool
+   */
   protected function isSecurityChip()
   {
     $bridge = $this->getShellExec("/usr/sbin/system_profiler SPiBridgeDataType | /usr/bin/awk -F: '/Model Name/ { gsub(/.*: /,\"\"); print $0}'");
@@ -366,6 +442,11 @@ class AbstractUpdateConsole extends AbstractConsole
     return !empty($bridge);
   }
 
+  /**
+   * Determines if the MacOS Secure Boot feature is set to "full".
+   *
+   * @return bool
+   */
   protected function isSecureBoot()
   {
     $P = Process::fromShellCommandline("nvram 94b73556-2197-4702-82a8-3e1337dafbfb:AppleSecureBootPolicy | awk '{ print $2 }'");
@@ -373,7 +454,7 @@ class AbstractUpdateConsole extends AbstractConsole
 
     if ($P->isSuccessful())
     {
-      return (strpos($P->getOutput(), "%02") !== false);
+      return false !== strpos($P->getOutput(), '%02');
     }
 
     return false;
