955079322e79cfce8690b53906f23af4086146ee
AMJones <am@jonesiscoding.com>
2021-03-12 17:12:28 -0600

    Adds additional power information to the summary command.

--- src/Command/SummaryCommand.php
+++ src/Command/SummaryCommand.php
@@ -50,10 +50,41 @@ class SummaryCommand extends AbstractUpdateConsole
       $this->io()->info('Content Cache', 50)->msgln(!empty($summary['content_cache']) ? implode(',', $summary['content_cache']) : 'None');
       $this->io()->info('SUS Url', 50)->msgln($summary['sus_url'] ? $summary['sus_url'] : 'None');
       $this->io()->info('Free Disk Space', 50)->msgln($summary['disk_space'].'GiB');
-      $this->io()->info('Has T2 Security Chip', 50)->msgln($summary['security_chip'] ? 'Yes' : 'No');
       $this->io()->blankln();
 
-      // Battery Power
+      // Battery Minutes
+      $this->io()->info('Battery Remaining', 50);
+
+      if ($summary['battery_minutes'] && $summary['battery_minutes'] > 60)
+      {
+        $this->io()->successln($summary['battery_minutes'].'mins');
+      }
+      elseif ($summary['battery_minutes'])
+      {
+        $suffix = ($summary['battery_minutes'] > 1) ? ' mins' : ' min';
+        $this->io()->errorln($summary['battery_minutes'].$suffix);
+      }
+      else
+      {
+        $this->io()->msgln('N/A');
+      }
+
+      // Battery Percentage
+      $this->io()->info('Battery Percentage', 50);
+      if ($summary['battery_percent'] && $summary['battery_percent'] < 33)
+      {
+        $this->io()->errorln($summary['battery_percent'].'%');
+      }
+      elseif ($summary['battery_percent'])
+      {
+        $this->io()->successln($summary['battery_percent'].'%');
+      }
+      else
+      {
+        $this->io()->msgln('N/A');
+      }
+
+      // On Battery Power?
       $this->io()->info('On Battery Power?', 50);
       if ($summary['battery'])
       {
@@ -63,6 +94,7 @@ class SummaryCommand extends AbstractUpdateConsole
       {
         $this->io()->successln('No');
       }
+      $this->io()->blankln();
 
       // Encryption in Progress
       $this->io()->info('Encryption in Progress?', 50);
@@ -111,21 +143,25 @@ class SummaryCommand extends AbstractUpdateConsole
   protected function getSummary($Updates)
   {
     $susUrl = $this->getDevice()->getOs()->getSoftwareUpdateCatalogUrl();
+    $onBatt = $this->isBatteryPowered();
+    $isBatt = $this->getDevice()->getBattery()->isInstalled();
+
     $output = [
-        'count'          => count($Updates),
-        'recommended'    => 0,
-        'restart'        => 0,
-        'shutdown'       => 0,
-        'battery'        => $this->isBatteryPowered(),
-        'console_user'   => $this->getConsoleUser(),
-        'console_userid' => $this->getConsoleUserId(),
-        'disk_space'     => $this->getDevice()->getFreeDiskSpace(),
-        'encrypting'     => $this->isEncryptingFileVault(),
-        'prevent_sleep'  => $this->isDisplaySleepPrevented(),
-        'security_chip'  => $this->isSecurityChip(),
-        'sus_offline'    => !$this->isSusAvailable($susUrl),
-        'sus_url'        => $this->getDevice()->getOs()->getSoftwareUpdateCatalogUrl(),
-        'content_cache'  => $this->getDevice()->getOs()->getSharedCaches(),
+        'count'           => count($Updates),
+        'recommended'     => 0,
+        'restart'         => 0,
+        'shutdown'        => 0,
+        'battery'         => $onBatt,
+        'battery_percent' => $isBatt ? $this->getDevice()->getBattery()->getPercentage() : null,
+        'battery_minutes' => $onBatt ? $this->getDevice()->getBattery()->getUntilEmpty('%i') : null,
+        'console_user'    => $this->getConsoleUser(),
+        'console_userid'  => $this->getConsoleUserId(),
+        'disk_space'      => $this->getDevice()->getFreeDiskSpace(),
+        'encrypting'      => $this->isEncryptingFileVault(),
+        'prevent_sleep'   => $this->isDisplaySleepPrevented(),
+        'sus_offline'     => !$this->isSusAvailable($susUrl),
+        'sus_url'         => $this->getDevice()->getOs()->getSoftwareUpdateCatalogUrl(),
+        'content_cache'   => $this->getDevice()->getOs()->getSharedCaches(),
     ];
 
     foreach ($Updates as $Update)
